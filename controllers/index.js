18d56cb0ec8883562763690d785f52ea01f3076c
itsDenMat <dennismateo93@gmail.com>
2022-09-20 21:05:20 -0700

    Update index.js in controllers folder

--- controllers/index.js
+++ controllers/index.js
@@ -2,8 +2,14 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
+const calendarRoutes = require('./calendarRoutes')
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
+router.use('/calendar', calendarRoutes);
+
+router.use((req, res) => {
+    res.status(404).end();
+});
 
 module.exports = router;
